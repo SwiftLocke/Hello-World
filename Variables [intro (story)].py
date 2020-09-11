fbf2144e9e0c6a7c3d9024ba9142eb218501984e
Ansh Makkar <67957023+SwiftLocke@users.noreply.github.com>
2020-09-11 19:12:59 +0100

    Add files via upload

---
+++ Variables [intro (story)].py
@@ -0,0 +1,7 @@
+character_name = "Marcelo"
+character_age = "21"
+print("There once was a man named " + character_name + ",")
+print("he was " + character_age + " years old. ")
+character_name = "Marc"
+print("He really liked the name " + character_name + ",")
+print("but he didn't like being " + character_age + ".")
